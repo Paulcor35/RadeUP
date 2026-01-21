a0298fd63811799606907b1d68b8f7def58285bc
Mathys1erDuNom <mathysbgames@gmail.com>
2026-01-21 17:32:26 +0100

    login avec BDD un peu propre

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,7 +5,7 @@ document.getElementById("loginForm").addEventListener("submit", async function (
     const password = document.getElementById("password").value;
 
     try {
-        const response = await fetch("bdd/users.json");
+        const response = await fetch("/RadeUp/BDD/users.json");
         const data = await response.json();
 
         const user = data.users.find(
