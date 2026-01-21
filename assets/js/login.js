a3a4ec5bdb2fef6de8dc51ec8b61e418e0c10c9f
Mathys1erDuNom <mathysbgames@gmail.com>
2026-01-21 17:29:12 +0100

    login avec BDD un peu propre

---
+++ assets/js/login.js
@@ -0,0 +1,27 @@
+document.getElementById("loginForm").addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const login = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        const response = await fetch("bdd/users.json");
+        const data = await response.json();
+
+        const user = data.users.find(
+            u => u.login === login && u.password === password
+        );
+
+        if (user) {
+            // Stocker l'utilisateur connecté
+            localStorage.setItem("user", JSON.stringify(user));
+            window.location.href = "index.html";
+        } else {
+            alert("❌ Login ou mot de passe incorrect");
+        }
+
+    } catch (error) {
+        console.error("Erreur de chargement BDD :", error);
+        alert("Erreur serveur");
+    }
+});
